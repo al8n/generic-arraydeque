d7bf4cb39841b676e90d5ad4b4f1678855c42d53
al8n <scygliu1@gmail.com>
2026-04-22 22:16:57 +1200

    chore: bumpup version and fix clippy

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
-#![deny(missing_docs, warnings)]
+#![deny(missing_docs)]
 
 #[cfg(all(not(feature = "std"), feature = "alloc"))]
 extern crate alloc as std;
